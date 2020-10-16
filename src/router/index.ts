7dc49264dc97ca4c35bea6215e3716fe43528b08
Yuri <yuri.swift.615@gmail.com>
2020-10-17 02:25:36 +0900

    ファイルパス修正

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 
-import Chat from './components/Chat.vue'
-import Room from './components/Room.vue'
-import AddRoom from './components/AddRoom.vue'
-import Login from './components/Login.vue'
+import Chat from '../components/Chat.vue';
+import Room from '../components/Room.vue';
+import AddRoom from '../components/AddRoom.vue';
+import Login from '../components/Login.vue';
 
 Vue.use(VueRouter);
 
