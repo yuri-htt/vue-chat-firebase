e82ea15c2f7d30c8b4cb87485fb43d8ecec402ab
Yuri <yuri.swift.615@gmail.com>
2020-10-16 17:51:35 +0900

    ルーティングの設定と画面のファイルのみ作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,34 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
+
+import Chat from './components/Chat.vue'
+import Room from './components/Room.vue'
+import AddRoom from './components/AddRoom.vue'
+import Login from './components/Login.vue'
 
 Vue.use(VueRouter);
 
 const routes: RouteConfig[] = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: '/chat/:nickname/:roomid/:roomname',
+    name: 'Chat',
+    component: Chat
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/room/:nickname',
+    name: 'RoomList',
+    component: Room
   },
+  {
+    path: '/add-room',
+    name: 'AddRoom',
+    component: AddRoom
+  },
+  {
+    path: '/',
+    name: 'Login',
+    component: Login
+  }
 ];
 
 const router = new VueRouter({
